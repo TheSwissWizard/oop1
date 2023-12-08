b2f247418f03e4cc1c08991634058d0c6be698d7
Florian Hug <florian.hug@unibe.ch>
2023-12-08 23:10:56 +0100

    refactor Morph

--- src/ch/fhnw/oop1/en2/game/entities/impl/Morph.java
+++ src/ch/fhnw/oop1/en2/game/entities/impl/Morph.java
@@ -1,14 +1,41 @@
 package ch.fhnw.oop1.en2.game.entities.impl;
 
 import ch.fhnw.oop1.en2.game.entities.ABubble;
-import ch.fhnw.oop1.en2.game.entities.MorphDNA;
 import ch.fhnw.oop1.en2.game.Renderer;
+import gui.Color;
+
 import java.util.Random;
 
 public class Morph extends ABubble {
 
   private enum MorphStates {
     META, KILLER, PREY
+  }
+
+  private enum MorphDNA {
+    TURQUOISE(1, new Color(6, 188, 156)),
+    GREEN(5, new Color(46, 204, 113)),
+    BLUE(10, new Color(52, 152, 219)),
+    VIOLET(20, new Color(155, 89, 182)),
+    YELLOW(40, new Color(241, 196, 15)),
+    ORANGE(70, new Color(230, 126, 34)),
+    RED(99, new Color(231, 76, 60));
+
+    private int points;
+    private Color color;
+
+    MorphDNA(int points, Color color) {
+      this.points = points;
+      this.color = color;
+    }
+
+    int getPoints() {
+      return this.points;
+    }
+
+    Color getColor() {
+      return this.color;
+    }
   }
 
   private MorphStates currentMorphState = MorphStates.META;
@@ -18,8 +45,6 @@ public class Morph extends ABubble {
   }
 
   public static Morph createNewMorph() {
-
-    // TODO random speed and direction
 
     MorphDNA dna = MorphDNA.values()[(int) (Math.random() * MorphDNA.values().length)];
     int x = new Random().nextInt((int) Renderer.getInstance().getWindow().getWidth()) + 1;
